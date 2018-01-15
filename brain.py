1e2c1d64d36cff6f5c2833924bc334adda0c4abb
James Marshall Reber <jmarshallreber@gmail.com>
2018-01-15 01:42:39 -0500

    added imgur

--- brain.py
+++ brain.py
@@ -1,15 +1,27 @@
 import groupy
 import time
+import requests
+import pyimgur
+
+IMGUR_ACCESS = ''
+
 
 def process_message(message):
     message = message.lower().split()
     if message[0] == '!latex':
         message = ''.join(message[1:])
-        return 'http://latex.codecogs.com/png.latex?\dpi{{300}}' + message
+        url ='http://latex.codecogs.com/gif.latex? ' + message
+        r = requests.get(url)
+        f = open('test.gif', 'wb')
+        f.write(r.content)
+        f.close()
+        im = pyimgur.Imgur(IMGUR_ACCESS)
+        uploaded_image = im.upload_image('test.gif', title="latex bot")
+        return uploaded_image.link
     return None
 
-group_name = 'gr'
-bot_name = ''
+group_name = 'Test'
+bot_name = 'latexbot'
 
 if __name__ == '__main__':
     index = 0
@@ -23,22 +35,14 @@ if __name__ == '__main__':
 
     index = 0
     bots = groupy.Bot.list()
-    try:
-        for i in range(len(bots)):
-            if bot_name in bots[i]:
-                index = i
-        bot = bots[index]
-    except:
-        bot = groupy.Bot.create(bot_name, group)
-        bot.post("I'm alive!")
+    for i in range(len(bots)):
+        if bot_name in str(bots[i]).split():
+            index = i
+    bot = bots[index]
 
     # Loop to continuously run
     while True:
-        try:
-            x = process_message(group.messages().newest.text)
-            if x != None:
-                bot.post(x)
-        except:
-            pass
-
+        x = process_message(group.messages().newest.text)
+        if x != None:
+            bot.post(x)
         time.sleep(3)
